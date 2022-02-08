acd22c75391252e623f7749f813d49d24de0cdf8
Mason Davy <mason.davy@etegent.com>
2022-02-08 10:50:32 -0500

    Fixed tabs

--- Utilities.cs
+++ Utilities.cs
@@ -6,7 +6,7 @@ namespace jfc {
         /// <summary> Determines if two strings are equal </summary>
         /// <param name="s0"> The first string to compare </param>
         /// <param name="s1"> The second string to compare </param>
-	/// <returns> Whether or not the two strings are equivalent </returns>
+        /// <returns> Whether or not the two strings are equivalent </returns>
         public bool Equals(string s0, string s1) {
             if (s0 == null && s1 == null) return true;
             if (s0 == null || s1 == null) return false;
@@ -14,8 +14,8 @@ namespace jfc {
         }
 
         /// <summary> Gets the hash code for a string </summary>
-	/// <param name="s"> The string whose hash code is to be computed </param>
-	/// <returns> The hash code of the string </returns>
+        /// <param name="s"> The string whose hash code is to be computed </param>
+        /// <returns> The hash code of the string </returns>
         public int GetHashCode(string s) => s?.GetHashCode() ?? 0;
     }
 }
