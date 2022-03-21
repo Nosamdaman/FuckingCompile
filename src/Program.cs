d13126c1e47bd788a0849be487de54c39247838b
Mason Davy <masonnosam11@gmail.com>
2022-03-21 13:45:24 -0400

    Updated the version number

--- src/Program.cs
+++ src/Program.cs
@@ -17,7 +17,7 @@ namespace jfc {
             "                        \"2\". Defaults to \"0\".\n" +
             "  -h --help             Show this screen\n" +
             "  -v --version          Show version information\n";
-        private const string _versionText = "v0.2.0";
+        private const string _versionText = "v0.3.0";
 
         /// <summary> Main entry-point function </summary>
         /// <param name="args"> Ordered list of all command-line arguments passed in </param>
