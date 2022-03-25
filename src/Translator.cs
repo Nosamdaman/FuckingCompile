0e8a7fad1ffcc8866ebbd5ba68f7bea15cdb0d8e
Mason Davy <mason.davy@etegent.com>
2022-03-25 08:59:41 -0400

    Added one thing

--- src/Translator.cs
+++ src/Translator.cs
@@ -3,5 +3,6 @@ using System.Text;
 namespace jfc {
     /// <summary> Class responsible for translating the source code to LLVM IR </summary>
     public class Translator {
+        private readonly StringBuilder _globals = new();
     }
 }
